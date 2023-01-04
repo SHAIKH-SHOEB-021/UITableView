1d04b97a45ffa98e873dd07f54ca1316e3085e4e
vinaygupta.cont <vinaygupta.cont@niveussolutions.com>
2023-01-04 12:13:30 +0530

    Add Details Text Label

--- UITableView/ViewController.swift
+++ UITableView/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let arrData = ["I-Phone","Samsung","Xiaomi","Nokia","Jio","Moto","Realme","OnePlus","GooglePixel","Hawai","Asus","Horner","Oppo","Vivo","Techno"]
+    let arrVlaue = ["Rs 10","Rs 5","Rs 30","Rs 60","Rs 40","Rs 20","Rs 50","Rs 10","Rs 20","Rs 50","Rs 5","Rs 10","Rs 40","Rs 30","Rs 20"]
     var tableViewUI : UITableView!
     
     override func viewDidLoad() {
@@ -32,8 +33,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return arrData.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let tableViewCell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        var tableViewCell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        tableViewCell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
         tableViewCell.textLabel?.text = arrData[indexPath.row]
+        tableViewCell.detailTextLabel?.text = arrVlaue[indexPath.row]
         return tableViewCell
     }
 }
